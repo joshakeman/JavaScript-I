aacab8499c26dbce40feebfec7b4b83cbe50e951
Josh Akeman <joshakeman@gmail.com>
2019-02-25 16:47:52 -0500

    commit

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,40 @@
 // };
 // myFunction();
 
+function myFunction () {
+ console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+var anotherFunction = (param) => {
+  return param;
+};
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+var add = (param1, param2) => {
+  return param1 + param2;
+};
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+var subtract = (param1, param2) => {
+  return param1 - param2;
+};
+subtract(1,2);
 
 
 // Stretch
